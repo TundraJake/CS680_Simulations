61a7d4ca7fc960456ddaf1d4785fd73bcb11b0ff
develjacob <mcjacob61@gmail.com>
2017-03-04 12:55:28 -0900

    refactored, polymorphism implemented

--- Exams/Exam1/Employee.py
+++ Exams/Exam1/Employee.py
@@ -4,7 +4,11 @@ UAF CS 680 Discrete Event Simulation
 Employee Class
 '''
 import random as rand
-
+'''
+Stretch Goal - set employee poor and high quality employees,
+the rate at which they operate on average times. 
+Also add sick or healthy status.
+'''
 
 ''' Simulation controls the number of employees '''
 class Employee(object):
