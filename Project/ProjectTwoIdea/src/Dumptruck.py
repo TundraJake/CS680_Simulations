56e973f23d781aa6b52e213d5f9256acebcb6fc7
develjacob <mcjacob61@gmail.com>
2017-04-24 02:21:51 -0800

    dump added

--- Project/ProjectTwoIdea/src/Dumptruck.py
+++ Project/ProjectTwoIdea/src/Dumptruck.py
@@ -11,6 +11,15 @@ class Dumptruck(Vehicle.Vehicle):
 	def __init__(self, employeeMinTime, employeeMaxTime):
 		super().__init__(employeeMinTime, employeeMaxTime)
 		self.__load = 0
+		self.state = 0
+		self.states = {
+					
+					'Hauling':0,
+					'Loading':1,
+					'Loaded':2, 
+					'Dumping':3
+				
+				}
 
 	def load(self):
 		self.__load = 300
